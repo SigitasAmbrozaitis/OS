79f0d03c634044bc73e4ec8c05b7158b6943f5e8
Povilas Kulevičius <kulevicius.povilas@gmail.com>
2019-03-04 17:07:48 +0200

    Finnished command switch

---
+++ VM_RM/Project/src/sample/CCD.java
@@ -0,0 +1,33 @@
+/**
+ * Kanalu irenginio klase
+ * maybe yes maybe no maybe i dont know
+ * B-B-B-Bbookas
+ */
+package sample;
+
+public class CCD {
+    CCD(){}
+    private short regSB;
+    private short regDB;
+    private short regST;
+    private short regDT;
+    private short regSZ;
+
+
+    public void setRegSB(short regSB){this.regSB = regSB;}
+    public short getRegSB(){ return regSB;}
+
+    public void setRegDB(short regDB){this.regDB = regDB;}
+    public short getRegDB(){ return regDB;}
+
+    public void setRegST(short regST){this.regST = regST;}
+    public short getRegST(){ return regST;}
+
+    public void setRegDT(short regDT){this.regDT = regDT;}
+    public short getRegDT(){ return regDT;}
+
+    public void setRegSZ(short regSZ){this.regSZ = regSZ;}
+    public short getRegSZ(){ return regSZ;}
+
+    public void XCHGN(){}
+}
